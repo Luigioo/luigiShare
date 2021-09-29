d28fa68d45964d0ec5394ba2f80e2577fe2d7932
Luigioo3 <linluigio@gmail.com>
2021-09-29 17:06:17 +0100

    add onclick to url

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let storedName;
 document.addEventListener("DOMContentLoaded", event=>{
 
     const app = firebase.app();
@@ -13,7 +14,12 @@ document.addEventListener("DOMContentLoaded", event=>{
     myPost.onSnapshot(doc=>{
         const data = doc.data();
         let nameElem = document.getElementById('name');
-        nameElem.innerHTML = data.name;
+        storedName = data.name;
+        let displayName = storedName;
+        // if(displayName.length>8){
+        //     displayName = displayName.substring(0,8)+'...';
+        // }
+        nameElem.innerHTML = displayName;
     })
 
 });
@@ -23,3 +29,21 @@ function updateName(e){
     const myPost = db.collection('jojo').doc('muda');
     myPost.update({name: e.target.value});
 }
+
+function goToLink(){
+    const url = document.getElementById("name").innerHTML;
+    if(!isValidHttpUrl(url)) return;
+    window.open(url, '_blank');
+}
+
+function isValidHttpUrl(string) {
+    let url;
+    
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+  
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
